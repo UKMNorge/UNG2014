a0cbd71441b6567a0c94e35ff8b44104141b36e0
Marius Mandal <mariusmandal@gmail.com>
2013-12-09 14:10:31 +0100

    Basisfiler

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once('vendor/autoload.php');
+require_once('UKM/sql.class.php');
+
+
+$app = new Silex\Application(); 
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/twig',
+));
+
+
+$url = new StdClass;
+$url->base 		= 'http://'. $_SERVER['HTTP_HOST'].'/';
+$url->create 	= $url->base.'/';
+$url->send 		= $url->base.'send/';
+$url->sent		= $url->base.'sendt/';
+$url->print		= $url->base.'print/';
+
+$TWIG = array('url' => $url);
+
+$app->get('/', function() use($app, $TWIG) {
+	return $app['twig']->render( 'frontpage.twig.html', $TWIG );
+}); 
